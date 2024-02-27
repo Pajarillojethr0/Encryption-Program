d3fb79e6aa0a598c236e8d76917b10f911f9314f
Pajarillojethr0 <jethropajarillo04@gmail.com>
2024-02-27 21:28:15 +0800

    Add retrieve Key feature.

--- src/EncryptionProgram.java
+++ src/EncryptionProgram.java
@@ -82,7 +82,16 @@ public class EncryptionProgram {
 		
 	}
 	private void retrieveKey() {
+		System.out.println("Key: ");
 		
+		for(Character x: list) {
+			System.out.print(x);
+		}
+		System.out.println();
+		for(Character x: shuffledList) {
+			System.out.print(x);
+		}
+		System.out.println();
 	}
 	private void encryptKey() {
 		
